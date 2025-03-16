71c931b23b7a4766caac0646a83d9e99e2789f62
naman <namansoni8874@gmail.com>
2025-03-16 22:00:26 +0530

    change in client

--- src/client/appwite.ts
+++ src/client/appwite.ts
@@ -1,9 +1,12 @@
-import { Client, Storage } from "appwrite";
+import { Account, Client, Storage } from "appwrite";
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('67c9df760008a58cfb70');
 
 const storage = new Storage(client);
+const account = new Account(client);
+const session = await account.get();
+console.log("User Session:", session);
 
 export { storage };
